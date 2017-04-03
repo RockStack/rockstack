d4811ab233baffc0242ee9542d193ec9cd61eb76
Luis Gustavo S. Barreto <gustavosbarreto@gmail.com>
2017-04-03 17:11:14 -0300

    Add Process module
    Signed-off-by: Luis Gustavo S. Barreto <gustavosbarreto@gmail.com>

---
+++ src/modules/process.hpp
@@ -0,0 +1,30 @@
+#ifndef PROCESS_HPP
+#define PROCESS_HPP
+
+#include <QObject>
+#include <QMap>
+#include <QProcess>
+
+class Process: public QObject
+{
+    Q_OBJECT
+
+public:
+    explicit Process(QObject *parent = 0);
+
+    qint64 exec(const QString &program, const QStringList &arguments);
+    void kill(qint64 pid);
+    void terminate(qint64 pid);
+    QByteArray readStandardOutput(qint64 pid);
+    QByteArray readStandardError(qint64 pid);
+
+signals:
+    void finished(qint64 pid, int exitCode, QProcess::ExitStatus exitStatus);
+    void standardOutput(qint64 pid, const QByteArray &data);
+    void standardError(qint64 pid, const QByteArray &data);
+
+private:
+    QMap<qint64, QProcess *> m_processes;
+};
+
+#endif // PROCESS_HPP
